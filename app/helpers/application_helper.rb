966c20354252ee7e42a37ff90fbab0df46590941
Kevin McPhillips <github@kevinmcphillips.ca>
2013-09-02 20:40:56 -0500

    Refactor title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def page_title
+    content_for?(:title) ? yield(:title) : "Expenses"
+  end
+
 end
---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+describe ApplicationHelper do
+
+  describe "#page_title" do
+    it "should be tested" do
+      pending
+    end
+  end
+
+end
