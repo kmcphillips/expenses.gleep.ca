1ef51ec2332500dc02c13b7593ccd2c931aed220
Kevin McPhillips <github@kevinmcphillips.ca>
2013-09-07 22:13:11 -0500

    Households need to be present for emails

--- app/models/authorized_email.rb
+++ app/models/authorized_email.rb
@@ -2,5 +2,6 @@ class AuthorizedEmail < ActiveRecord::Base
   belongs_to :household
 
   validates :email, presence: true, uniqueness: {scope: :household_id}
+  validates :household, presence: true
 
 end
