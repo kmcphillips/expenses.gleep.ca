231b7914b6b76e7cb0d4df1e57b39d3043ee4f32
Kevin McPhillips <github@kevinmcphillips.ca>
2015-02-16 18:23:20 -0500

    Add a read-only mode

--- app/controllers/authenticated_controller.rb
+++ app/controllers/authenticated_controller.rb
@@ -1,7 +1,9 @@
 class AuthenticatedController < ApplicationController
-  before_action :authenticate_user!, :reset_login_token_session
+  before_action :authenticate_user!, :reset_login_token_session, :check_read_only
 
   helper_method :current_household
+
+  protected
 
   def current_household
     current_user.try(:household)
@@ -9,6 +11,20 @@ class AuthenticatedController < ApplicationController
 
   def reset_login_token_session
     session[:login_token_prevent_redirect] = nil
+  end
+
+  def read_only?
+    !!Rails.application.secrets[:read_only]
+  end
+
+  def check_read_only
+    if read_only?
+      sign_out current_user
+      flash[:notice] = "You cannot login at this time. Site temporarily in read-only mode."
+      redirect_to new_user_session_path
+    end
+
+    true
   end
 
 end
